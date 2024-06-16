18e0284f8cb29cd54d79bf4b53c0df62f2e72205
OzNL <140854571+OzNL-Hub@users.noreply.github.com>
2024-06-16 03:14:46 +0300

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 4855457388 or 5094651510 then -- Demonfall
+
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/OzNL-Hub/OzNL-HUB-Regular/main/Demonfall.lua"))()
+
+end
