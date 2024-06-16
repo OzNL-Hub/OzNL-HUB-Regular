9b2bf878528cbd358ae0d62d3966273e9a36f24a
OzNL <140854571+OzNL-Hub@users.noreply.github.com>
2024-06-16 03:18:05 +0300

    Update Demonfall.lua

--- Demonfall.lua
+++ Demonfall.lua
@@ -1,1 +1,36 @@
-print
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/OzNL-Hub/OzNL-Hub/main/Library.lua"))()
+
+local PepsisWorld = library:CreateWindow({
+    Name = "OzNL Hub",
+    Defult = false,
+    Themeable = {
+        Info = 'By: OzNL',
+        Background  = 0,
+        Credit = false,
+    }
+})
+
+local GeneralTab = PepsisWorld:CreateTab({
+    Name = "General"
+})
+
+local MiscellaneousSection = GeneralTab:CreateSection({
+    Name = "Miscellaneous",
+    Side = "Left"
+})
+
+MiscellaneousSection:AddButton({
+    Name = "Godmode",
+    Callback = function()
+        pcall(function()
+            game:GetService("ReplicatedStorage").Remotes.Async:FireServer("Character", "FallDamageServer", 0/0)
+        end)
+    end
+})
+
+MiscellaneousSection:AddButton({
+    Name = "Normal Health (Kills you)",
+    Callback = function()
+        game:GetService("ReplicatedStorage").Remotes.Async:FireServer("Character", "FallDamageServer", 99999)
+    end
+})
